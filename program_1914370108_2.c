021a6972b4cc6e161dc3b2de8da2934836561141
fadlirivansyah12 <55773490+fadlirivansyah12@users.noreply.github.com>
2019-11-19 08:25:07 +0700

    Add files via upload

---
+++ program_1914370108_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    for (int i =1 ; i <= 5; i++)
+    {
+        for (int h =1 ;h  <= i; h++)
+        {
+           printf("* ");
+        }
+          printf("\n");
+          }
+          for (int i =4 ; i >=1 ; i--)
+              {
+             for (int h =1 ;h <=i ; h++)
+                 {
+               printf("* ");
+        }
+                printf("\n");
+            }
+            return 0;
+            }
+
+
+
